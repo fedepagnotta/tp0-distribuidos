177f697223db816d1c3d696259a0281cf857aea7
fedepagnotta <fedepagnotta14@gmail.com>
2025-08-23 13:29:12 -0300

    feat: create script generar-compose.sh

---
+++ generar-compose.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo "Nombre del archivo de salida: $1"
+echo "Cantidad de clientes: $2"
+
+touch $1
+
+echo "name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net" > $1
+
+for i in $(seq 1 $2); do
+    echo "
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server" >> $1
+done
+
+echo "
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24" >> $1
