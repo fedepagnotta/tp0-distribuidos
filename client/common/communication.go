959204bfa56b2fcf7728ac1d028a631392d70609
fedepagnotta <fedepagnotta14@gmail.com>
2025-08-26 14:33:39 -0300

    feat: implement ReadFrom in client for BetsRecvSuccess and BetsRecvFail

--- client/common/communication.go
+++ client/common/communication.go
@@ -1,23 +1,32 @@
 package common
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"io"
 	"net"
 )
+
+const NewBetsOpCode byte = 0
+const BetsRecvSuccessOpCode byte = 1
+const BetsRecvFailOpCode byte = 2
+
+type ProtocolError struct {
+	Msg    string
+	Opcode byte
+}
+
+func (e *ProtocolError) Error() string {
+	return fmt.Sprintf("protocol error: %s (opcode=%d)", e.Msg, e.Opcode)
+}
 
 type Writeable interface {
 	// writes contents to out following the package format (opcode, length, body)
 	// returns the total length of the body, or error if the write failed
 	WriteTo(out net.Conn) (int, error)
 }
-
-type NewBets struct {
-	Bets []map[string]string
-}
-
-const NewBetsOpCode byte = 0
 
 func writeString(buff *bytes.Buffer, s string) error {
 	if err := binary.Write(buff, binary.LittleEndian, int32(len(s))); err != nil {
@@ -34,18 +43,22 @@ func writePair(buff *bytes.Buffer, k string, v string) error {
 	return writeString(buff, v)
 }
 
-func writeMultiStringMap(buff *bytes.Buffer, bets NewBets) error {
-	for _, bet := range bets.Bets {
-		if err := binary.Write(buff, binary.LittleEndian, int32(len(bet))); err != nil {
+func writeMultiStringMap(buff *bytes.Buffer, body []map[string]string) error {
+	for _, m := range body {
+		if err := binary.Write(buff, binary.LittleEndian, int32(len(m))); err != nil {
 			return err
 		}
-		for k, v := range bet {
+		for k, v := range m {
 			if err := writePair(buff, k, v); err != nil {
 				return err
 			}
 		}
 	}
 	return nil
+}
+
+type NewBets struct {
+	Bets []map[string]string
 }
 
 func (b NewBets) WriteTo(out net.Conn) (int, error) {
@@ -57,7 +70,7 @@ func (b NewBets) WriteTo(out net.Conn) (int, error) {
 	if err := binary.Write(&bodyBuff, binary.LittleEndian, int32(len(b.Bets))); err != nil {
 		return 0, err
 	}
-	if err := writeMultiStringMap(&bodyBuff, b); err != nil {
+	if err := writeMultiStringMap(&bodyBuff, b.Bets); err != nil {
 		return 0, err
 	}
 	if err := binary.Write(&buff, binary.LittleEndian, int32(bodyBuff.Len())); err != nil {
@@ -72,4 +85,57 @@ func (b NewBets) WriteTo(out net.Conn) (int, error) {
 		return 0, err
 	}
 	return bodyBuff.Len(), nil
+}
+
+type Readable interface {
+	readFrom(reader *bufio.Reader) (Readable, error)
+}
+
+type BetsRecvSuccess struct{}
+
+func (msg *BetsRecvSuccess) readFrom(reader *bufio.Reader) (Readable, error) {
+	var length int32
+	if err := binary.Read(reader, binary.LittleEndian, &length); err != nil {
+		return nil, err
+	}
+	if length != 0 {
+		return nil, &ProtocolError{"invalid body length", BetsRecvSuccessOpCode}
+	}
+	return msg, nil
+}
+
+type BetsRecvFail struct{}
+
+func (msg *BetsRecvFail) readFrom(reader *bufio.Reader) (Readable, error) {
+	var length int32
+	if err := binary.Read(reader, binary.LittleEndian, &length); err != nil {
+		return nil, err
+	}
+	if length != 0 {
+		return nil, &ProtocolError{"invalid body length", BetsRecvFailOpCode}
+	}
+	return msg, nil
+}
+
+// reads contents from reader and returns the parsed package
+func ReadMessage(reader *bufio.Reader) (Readable, error) {
+	var opcode byte
+	var err error
+	if opcode, err = reader.ReadByte(); err != nil {
+		return nil, err
+	}
+	switch opcode {
+	case BetsRecvSuccessOpCode:
+		{
+			var msg BetsRecvSuccess
+			return msg.readFrom(reader)
+		}
+	case BetsRecvFailOpCode:
+		{
+			var msg BetsRecvFail
+			return msg.readFrom(reader)
+		}
+	default:
+		return nil, &ProtocolError{"invalid opcode", opcode}
+	}
 }
