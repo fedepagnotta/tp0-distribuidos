bf58ad8c855deb8744a7cf0b5c2de6888b49afc6
fedepagnotta <fedepagnotta14@gmail.com>
2025-08-29 23:55:29 -0300

    feat: update server's __handle_client_connection to improve batch handling

--- server/common/server.py
+++ server/common/server.py
@@ -34,30 +34,48 @@ class Server:
         logging.shutdown()
 
     def __handle_client_connection(self, client_sock):
-        """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
-        try:
-            msg = communication.recv_msg(client_sock)
-            addr = client_sock.getpeername()
-            logging.info(
-                f"action: receive_message | result: success | ip: {addr[0]} | msg: {msg}"
-            )
-            response = communication.BetsRecvSuccess()
-            response.write_to(client_sock)
-            msg.process()
-        except (EOFError, communication.ProtocolError) as e:
-            logging.error("action: receive_message | result: fail | error: %s", e)
-            response = communication.BetsRecvFail()
+        while True:
+            msg = None
             try:
-                response.write_to(client_sock)
-            except communication.ProtocolError as e1:
-                logging.error("action: send_message | result: fail | error: %s", e1)
-        finally:
-            client_sock.close()
+                msg = communication.recv_msg(client_sock)
+                addr = client_sock.getpeername()
+                logging.info(
+                    "action: receive_message | result: success | ip: %s | opcode: %i",
+                    addr[0],
+                    msg.opcode,
+                )
+
+                try:
+                    msg.process()
+                except Exception as e:
+                    logging.error("action: process_bets | result: fail | error: %s", e)
+                    communication.BetsRecvFail().write_to(client_sock)
+                    logging.error(
+                        "action: apuesta_recibida | result: fail | cantidad: %i",
+                        getattr(msg, "amount", 0),
+                    )
+                    continue
+
+                communication.BetsRecvSuccess().write_to(client_sock)
+                logging.info(
+                    "action: apuesta_recibida | result: success | cantidad: %i",
+                    msg.amount,
+                )
+
+            except communication.ProtocolError as e:
+                try:
+                    communication.BetsRecvFail().write_to(client_sock)
+                except Exception as e1:
+                    logging.error("action: send_message | result: fail | error: %s", e1)
+                logging.error("action: apuesta_recibida | result: fail")
+                logging.error("action: receive_message | result: fail | error: %s", e)
+            except EOFError:
+                break
+            except OSError as e:
+                logging.error("action: send_message | result: fail | error: %s", e)
+                break
+
+        client_sock.close()
 
     def __accept_new_connection(self):
         """
