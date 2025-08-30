82692966d890282b8e2346b3dc81b032bb93a35d
fedepagnotta <fedepagnotta14@gmail.com>
2025-08-30 20:05:55 -0300

    feat: update server to respect new behavior

--- server/common/server.py
+++ server/common/server.py
@@ -12,6 +12,9 @@ class Server:
         self._server_socket.bind(("", port))
         self._server_socket.listen(listen_backlog)
         self._running = False
+        self._finished: set[int] = set()
+        self._winners: dict[int, list[str]] = {}
+        self._raffle_done: bool = False
 
     def run(self):
         """
@@ -33,6 +36,63 @@ class Server:
                 raise
         logging.shutdown()
 
+    def __raffle(self):
+        try:
+            bets = utils.load_bets()
+            winners = [(b.agency, b.document) for b in bets if utils.has_won(b)]
+            for w in winners:
+                self._winners.setdefault(w[0], []).append(w[1])
+            logging.info("action: sorteo | result: success")
+            self._raffle_done = True
+        except Exception as e:
+            logging.error("action: sorteo | result: fail | error: %s", e)
+            return
+
+    def __send_winners(self, agency_id, sock):
+        try:
+            communication.Winners(self._winners.get(agency_id, [])).write_to(sock)
+            logging.info(
+                "action: enviar_ganadores | result: success | agencia: %d", agency_id
+            )
+        except communication.ProtocolError as e:
+            logging.error(
+                "action: enviar_ganadores | result: fail | agencia: %d | error: %s",
+                agency_id,
+                e,
+            )
+
+    def __process_msg(self, msg, client_sock: socket.socket) -> bool:
+        if msg.opcode == communication.Opcodes.NEW_BETS:
+            try:
+                utils.store_bets(msg.bets)
+                for bet in msg.bets:
+                    logging.info(
+                        "action: apuesta_almacenada | result: success | dni: %s | numero: %s",
+                        bet.document,
+                        bet.number,
+                    )
+            except Exception as e:
+                communication.BetsRecvFail().write_to(client_sock)
+                logging.error(
+                    "action: apuesta_recibida | result: fail | cantidad: %d", msg.amount
+                )
+                return True
+            logging.info(
+                "action: apuesta_recibida | result: success | cantidad: %d",
+                msg.amount,
+            )
+            communication.BetsRecvSuccess().write_to(client_sock)
+            return True
+        if msg.opcode == communication.Opcodes.FINISHED:
+            self._finished.add(msg.agency_id)
+            if len(self._finished) == 5 and not self._raffle_done:
+                self.__raffle()
+            return False
+        if msg.opcode == communication.Opcodes.REQUEST_WINNERS:
+            if self._raffle_done and msg.agency_id in self._finished:
+                self.__send_winners(msg.agency_id, client_sock)
+            return False
+
     def __handle_client_connection(self, client_sock):
         while True:
             msg = None
@@ -44,38 +104,15 @@ class Server:
                     addr[0],
                     msg.opcode,
                 )
-
-                try:
-                    msg.process()
-                except Exception as e:
-                    logging.error("action: process_bets | result: fail | error: %s", e)
-                    communication.BetsRecvFail().write_to(client_sock)
-                    logging.error(
-                        "action: apuesta_recibida | result: fail | cantidad: %i",
-                        getattr(msg, "amount", 0),
-                    )
-                    continue
-
-                communication.BetsRecvSuccess().write_to(client_sock)
-                logging.info(
-                    "action: apuesta_recibida | result: success | cantidad: %i",
-                    msg.amount,
-                )
-
+                if not self.__process_msg(msg, client_sock):
+                    break
             except communication.ProtocolError as e:
-                try:
-                    communication.BetsRecvFail().write_to(client_sock)
-                except Exception as e1:
-                    logging.error("action: send_message | result: fail | error: %s", e1)
-                logging.error("action: apuesta_recibida | result: fail")
                 logging.error("action: receive_message | result: fail | error: %s", e)
             except EOFError:
-                client_sock.close()
                 break
             except OSError as e:
                 logging.error("action: send_message | result: fail | error: %s", e)
                 break
-
         client_sock.close()
 
     def __accept_new_connection(self):
